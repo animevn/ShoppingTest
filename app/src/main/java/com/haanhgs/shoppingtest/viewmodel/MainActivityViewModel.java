131636ef938e24a1d08c4f1f21a1d3d6283b5cfa
animevn <haanhth21@gmail.com>
2019-11-23 00:06:21 +0700

    good one for a day, all tomorrow should do

---
+++ app/src/main/java/com/haanhgs/shoppingtest/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,32 @@
+package com.haanhgs.shoppingtest.viewmodel;
+
+import com.haanhgs.shoppingtest.Filters;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+
+    private boolean mIsSigningIn;
+    private Filters mFilters;
+
+    public MainActivityViewModel() {
+        mIsSigningIn = false;
+        mFilters = Filters.getDefault();
+    }
+
+    public boolean getIsSigningIn() {
+        return mIsSigningIn;
+    }
+
+    public void setIsSigningIn(boolean mIsSigningIn) {
+        this.mIsSigningIn = mIsSigningIn;
+    }
+
+    public Filters getFilters() {
+        return mFilters;
+    }
+
+    public void setFilters(Filters mFilters) {
+        this.mFilters = mFilters;
+    }
+}
